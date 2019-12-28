50d4874841f9bcbc063d2522b2de9a9547c7efa2
Peter Clayder <peterclayder@gmail.com>
2019-12-28 11:13:24 -0300

    Model Supplier

---
+++ Models/Supplier.cs
@@ -0,0 +1,13 @@
+namespace dotnet_sistema_delivery.Models
+{
+    public class Supplier
+    {
+        private int id { get; set; }
+
+        private string name { get; set; }
+
+        private string email { get; set; }
+
+        private string telephone { get; set; }
+    }
+}
