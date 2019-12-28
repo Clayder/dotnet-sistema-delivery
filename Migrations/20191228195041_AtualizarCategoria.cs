13cbef4f5a4ae3a77cda7fbd530bc1d4c18e433e
Peter Clayder <peterclayder@gmail.com>
2019-12-28 16:51:57 -0300

    Add coluna status na tabela categoria.

---
+++ Migrations/20191228195041_AtualizarCategoria.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnet_sistema_delivery.Migrations
+{
+    public partial class AtualizarCategoria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "status",
+                table: "category",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "category");
+        }
+    }
+}
