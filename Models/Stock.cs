a80b6b15a2d1db7dfd9deae3b2e9061508ff580a
Peter Clayder <peterclayder@gmail.com>
2019-12-28 11:13:08 -0300

    Model Stock

---
+++ Models/Stock.cs
@@ -0,0 +1,11 @@
+namespace dotnet_sistema_delivery.Models
+{
+    public class Stock
+    {
+        private int id { get; set; }
+
+        private Product product { get; set; }
+
+        private int qty { get; set; }
+    }
+}
